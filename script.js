3d257b20f5f7215be67658fd958cf6c727a15e09
ahmedhosseny <109736696+ahmedhosseny@users.noreply.github.com>
2023-05-02 17:18:55 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function add() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var result = num1 + num2;
+    document.getElementById("result").innerHTML = result;
+  }
+  
+  function subtract() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var result = num1 - num2;
+    document.getElementById("result").innerHTML = result;
+  }
+  
+  function clear1() {
+    document.getElementById("num1").value = "";
+    document.getElementById("num2").value = "";
+    document.getElementById("result").innerHTML = "";
+  }
+  
